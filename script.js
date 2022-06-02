43d43ec1b6b1a27efc185a0400832a184f392093
taran9999 <taran184@gmail.com>
2022-06-01 19:40:46 -0700

    Added an onmouseover event that changes a grid square to black when the mouse hovers over it. Also added a reset button which changes each square back to white

--- script.js
+++ script.js
@@ -7,6 +7,14 @@ for(let i = 0; i < 16; i++) {
     for(let j = 0; j < 16; j++) {
         let g = document.createElement('div');
         g.classList.add("grid");
+        g.onmouseover = () => {g.setAttribute('style', 'background-color: black; width: calc(75vh / 16); height: calc(75vh / 16); border: solid black;')};
         r.appendChild(g);
+    }
+}
+
+document.getElementById("reset").onclick = () => {
+    let gridSquares = document.querySelectorAll(".grid");
+    for(let i = 0; i < gridSquares.length; i++) {
+        gridSquares[i].setAttribute('style', 'background-color: ; width: calc(75vh / 16); height: calc(75vh / 16); border: solid black;');
     }
 }
