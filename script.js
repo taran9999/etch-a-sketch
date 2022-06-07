48cdc59a75660cac647ef911e9615017a36d6d82
taran9999 <taran184@gmail.com>
2022-06-07 15:28:32 -0700

    Created functions to draw the grid of any size, and clear all elements from the grid. Moved grid styling from the css file to the draw function in the script.

--- script.js
+++ script.js
@@ -1,25 +1,48 @@
 const gridContainer = document.getElementById('grid-container');
 
-for(let i = 0; i < 16; i++) {
-    let r = document.createElement('div');
-    r.classList.add("grid-rows");
-    gridContainer.appendChild(r);
-    for(let j = 0; j < 16; j++) {
-        let g = document.createElement('div');
-        g.classList.add("grid");
+drawGrid(16);
 
-        if(j == 15) {
-            g.classList.add("grid-right-col");
-        }
-        
-        g.onmouseover = () => {g.style.backgroundColor = "black"};
-        r.appendChild(g);
-    }
+document.getElementById("reset").onclick = () => {document.querySelectorAll(".grid").forEach((gridSquare) => gridSquare.style.backgroundColor = "");}
+
+function clearGrid() {
+    document.querySelectorAll(".grid-rows").forEach((r) => r.remove());
+    document.querySelectorAll(".grid").forEach((g) => g.remove());
 }
 
-document.getElementById("reset").onclick = () => {
-    let gridSquares = document.querySelectorAll(".grid");
-    for(let i = 0; i < gridSquares.length; i++) {
-        gridSquares[i].style.backgroundColor = "";
+function drawGrid(n) {
+    //create n grid rows, and add n squares in each row, making a n x n grid.
+    for(let i = 0; i < n; i++) {
+        let r = document.createElement('div');
+        r.classList.add("grid-rows");
+        gridContainer.appendChild(r);
+        for(let j = 0; j < n; j++) {
+            let g = document.createElement('div');
+            g.classList.add("grid");
+    
+            if(j == n - 1) {
+                g.classList.add("grid-right-col");
+            }
+            
+            g.onmouseover = () => {g.style.backgroundColor = "black"};
+            r.appendChild(g);
+        }
     }
+
+    //change the size of each grid row and grid square, based on the number of squares.
+    document.querySelectorAll(".grid-rows").forEach((r) => r.setAttribute('style', 
+    `display: flex;
+    width: 75vh;
+    height: calc(75vh / ${n});`));
+
+    document.querySelectorAll(".grid").forEach((g) => g.setAttribute('style', 
+    `width: calc(75vh / ${n});
+    height: calc(75vh / ${n});
+    border-right: solid black;
+    border-bottom: solid black;`));
+
+    //prevent overlap of borders on the right side
+    document.querySelectorAll(".grid-right-col").forEach((g) => g.setAttribute('style', 
+    `width: calc(75vh / ${n});
+    height: calc(75vh / ${n});
+    border-bottom: solid black;`));
 }
